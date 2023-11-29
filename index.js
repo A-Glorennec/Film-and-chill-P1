5fe1e772803f798ceaa98ff2ef5e29859a60f864
ANAIS GLORENNEC <zans_69@hotmail.fr>
2023-11-29 15:05:03 +0100

    categories desktop layout

--- index.js
+++ index.js
@@ -1,3 +1,371 @@
+const cards = [
+    {
+        id: 1,
+        categorie: "Drame",
+        items: [
+            {
+                id: 1,
+                title: "The Dark Knight",
+                image: "assets/affiches-films/affiche-the-dark-knight.jpeg",
+                alt: "Affiche The Dark Knight",
+            },
+
+            {
+                id: 2,
+                title: "Forest Gump",
+                image: "assets/affiches-films/affiche-forest-gump.jpg",
+                alt: "Forest Gump",
+            },
+            {
+                id: 3,
+                title: "Me Before You",
+                image: "assets/affiches-films/affiche-me-before-you.jpg",
+                alt: "Affiche Me Before You",
+            },
+            {
+                id: 4,
+                title: "Babylone",
+                image: "assets/affiches-films/affiche-babylone.jpg",
+                alt: "Affiche Babylone"
+            },
+            {
+                id: 5,
+                title: "Split",
+                image: "assets/affiches-films/affiche-split.jpeg",
+                alt: "Affiche Split",
+            },
+            {
+                id: 5,
+                title: "Split",
+                image: "assets/affiches-films/affiche-split.jpeg",
+                alt: "Affiche Split",
+            },
+            {
+                id: 5,
+                title: "Split",
+                image: "assets/affiches-films/affiche-split.jpeg",
+                alt: "Affiche Split",
+            }
+        ]
+
+    },
+    {
+        id: 2,
+        categorie: "Fantastique",
+        items: [
+            {
+                id: 1,
+                title: "The hobbit",
+                image: "assets/affiches-films/affiche_bilbo.jpg",
+                alt: "Affiche The Hobbit",
+            },
+
+            {
+                id: 2,
+                title: "Le Magicien d'Oz",
+                image: "assets/affiches-films/affiche-oz.jpeg",
+                alt: "Affiche Le Magicien d'Oz",
+            },
+            {
+                id: 3,
+                title: "Harry Potter",
+                image: "assets/affiches-films/affiche-harry-potter.jpg",
+                alt: "Affiche Harry Potter",
+            },
+            {
+
+                id: 4,
+                title: "Narnia",
+                image: "assets/affiches-films/affiche-narnia.jpg",
+                alt: "Affiche Narnia",
+            },
+            {
+                id: 5,
+                title: "Le Seigneur des Anneaux",
+                image: "assets/affiches-films/affiche-seigneur-des-anneaux.jpg",
+                alt: "Affiche Le Seigneur des Anneaux",
+            },
+            {
+                id: 6,
+                title: "Le Secret de l'étoile du Nord",
+                image: "assets/affiches-films/le-secret-de-letoile-du-nord.jpg",
+                alt: "Affiche Le Secret de l'étoile du Nord",
+            },
+            {
+                id: 7,
+                title: "Le Secret de Dumbledore",
+                image: "assets/affiches-films/affiche-le-secret-de-dumbledor.jpeg",
+                alt: "Affiche Le Secret de Dumbledore",
+            },
+            {
+                id: 5,
+                title: "Le Seigneur des Anneaux",
+                image: "assets/affiches-films/affiche-seigneur-des-anneaux.jpg",
+                alt: "Affiche Le Seigneur des Anneaux",
+            }
+        ]
+
+    },
+    {
+        id: 3,
+        categorie: "Jeunesse",
+        items: [
+            {
+                id: 1,
+                title: "Amazonia",
+                image: "assets/affiches-films/affiche-amazonia.jpg",
+                alt: "Affiche Amazonia",
+            },
+            {
+                id: 2,
+                title: "Astérix et Obélix",
+                image: "assets/affiches-films/affiche-asterix-obelix.jpg",
+                alt: "Affiche Astérix et Obélix",
+            },
+            {
+                id: 3,
+                title: "Pôle Express",
+                image: "assets/affiches-films/affiche-pole-express.jpg",
+                alt: "Affiche Pôle Express",
+            },
+            {
+                id: 4,
+                title: "La Belle et la Bête",
+                image: "assets/affiches-films/affiche-belle-et-la-bete.jpg",
+                alt: "Affiche La Belle et la Bête",
+            },
+            {
+                id: 5,
+                title: "A la Croisée des Mondes",
+                image: "assets/affiches-films/affiche-la-croisee-des-mondes.jpg",
+                alt: "A la Croisée des Mondes",
+            },
+            {
+                id: 6,
+                title: "Tintin",
+                image: "assets/affiches-films/affiche-tintin.jpeg",
+                alt: "Affiche Tintin",
+            },
+            {
+                id: 7,
+                title: "Pat' Patrouille",
+                image: "assets/affiches-films/affiche-pat-patrouille.jpeg",
+                alt: "Affiche Pat' Patrouille",
+            },
+
+        ]
+
+    },
+    {
+        id: 4,
+        categorie: "Comédie",
+        items: [
+            {
+                id: 1,
+                title: "L'ascension",
+                image: "assets/affiches-films/affiche-ascension.jpg",
+                alt: "Affiche L'Ascension",
+            },
+            {
+                id: 2,
+                title: "La Cité de la Peur",
+                image: "assets/affiches-films/affiche-cite-de-la-peur.jpg",
+                alt: "Affiche La Cité de la Peur",
+            },
+            {
+                id: 3,
+                title: "Mine de Rien",
+                image: "assets/affiches-films/affiche-mine-de-rien.jpg",
+                alt: "Affiche Mine de Rien",
+            },
+            {
+                id: 4,
+                title: "Epouse Moi Mon Pote",
+                image: "assets/affiches-films/affiche-epouse-moi.jpg",
+                alt: "Affiche Epouse Moi Mon Pote",
+            },
+            {
+                id: 5,
+                title: "Les Complices",
+                image: "assets/affiches-films/affiche-les-complices.jpg",
+                alt: "Affiche Les Complices",
+            },
+            {
+                id: 6,
+                title: "Divorce Club",
+                image: "assets/affiches-films/affiche-divorce-club.jpg",
+                alt: "Affiche Divorce Club",
+            },
+
+
+        ]
+
+    },
+    {
+        id: 5,
+        categorie: "Action",
+        items: [
+            {
+                id: 1,
+                title: "Hacker",
+                image: "assets/affiches-films/affiche-hacker.png",
+                alt: "Affiche Hacker",
+            },
+            {
+                id: 2,
+                title: "Mad Max",
+                image: "assets/affiches-films/affiche-mad-max.jpeg",
+                alt: "Affiche Mad Max",
+            },
+            {
+                id: 3,
+                title: "Rocky Balboa",
+                image: "assets/affiches-films/affiche-rocky.jpeg",
+                alt: "Affiche Rocky Balboa",
+            },
+            {
+                id: 4,
+                title: "Terminator Salvation",
+                image: "assets/affiches-films/affiche-terminator.jpg",
+                alt: "Affiche Terminator Salvation",
+            },
+            {
+                id: 5,
+                title: "Expandables 2",
+                image: "assets/affiches-films/affiche-expandables-2.jpg",
+                alt: "Affiche Expandables 2",
+            },
+            {
+                id: 6,
+                title: "Blade Runner",
+                image: "assets/affiches-films/affiche-blade-runner.jpg",
+                alt: "Affiche Blade Runner",
+            },
+            {
+                id: 7,
+                title: "Skyfall",
+                image: "assets/affiches-films/affiche-skyfall.jpg",
+                alt: "Affiche Skyfall",
+            },
+
+
+        ]
+
+    },
+    {
+        id: 6,
+        categorie: "Romance",
+        items: [
+            {
+                id: 1,
+                title: "Il était temps",
+                image: "assets/affiches-films/affiche-il-etait-temps.jpg",
+                alt: "Affiche Il était temps",
+            },
+            {
+                id: 2,
+                title: "Me Before You",
+                image: "assets/affiches-films/affiche-me-before-you.jpg",
+                alt: "Affiche Me Before You",
+            },
+            {
+                id: 3,
+                title: "After",
+                image: "assets/affiches-films/affiche-after.jpeg",
+                alt: "Affiche After",
+            },
+            {
+                id: 4,
+                title: "Une Seconde Chance",
+                image: "assets/affiches-films/affiche-seconde-chance.jpeg",
+                alt: "Affiche Une Seconde Chance",
+            },
+            {
+                id: 5,
+                title: "One Day",
+                image: "assets/affiches-films/affiche-one-day.jpeg",
+                alt: "Affiche One Day",
+            },
+            {
+                id: 6,
+                title: "50 Nuances de Grey",
+                image: "assets/affiches-films/affiche-50-nuances-de-grey.jpeg",
+                alt: "50 Nuances de Grey",
+            },
+            {
+                id: 7,
+                title: "N'oublie Jamais",
+                image: "assets/affiches-films/affiche-oublie-jamais.jpeg",
+                alt: "Affiche N'oublie Jamais",
+            },
+            {
+                id: 8,
+                title: "N'oublie Jamais",
+                image: "assets/affiches-films/affiche-oublie-jamais.jpeg",
+                alt: "Affiche N'oublie Jamais",
+            },
+
+
+        ]
+
+    },
+    {
+        id: 7,
+        categorie: "Policier",
+        items: [
+
+            {
+                id: 1,
+                title: "Police",
+                image: "assets/affiches-films/affiche-police.jpeg",
+                alt: "Affiche Police",
+
+            },
+            {
+                id: 2,
+                title: "Colt 45",
+                image: "assets/affiches-films/affiche-colt-45.jpeg",
+                alt: "Affiche Colt 45",
+
+            },
+            {
+                id: 3,
+                title: "La Taupe",
+                image: "assets/affiches-films/affiche-la-taupe.jpeg",
+                alt: "Affiche La Taupe",
+
+            },
+            {
+                id: 4,
+                title: "Legend",
+                image: "assets/affiches-films/affiche-legend.jpeg",
+                alt: "Affiche Legend",
+
+            },
+            {
+                id: 5,
+                title: "Contre Enquête",
+                image: "assets/affiches-films/affiche-contre-enquete.jpeg",
+                alt: "Affiche Contre Enquête",
+
+            },
+            {
+                id: 6,
+                title: "Limbo",
+                image: "assets/affiches-films/affiche-limbo.jpeg",
+                alt: "Affiche Limbo",
+
+            },
+
+
+        ]
+
+    },
+
+];
+
+
+
 const sideNav = document.querySelector("#my-sidenav");
 const openBtn = document.querySelector("#open-btn");
 const closeBtn = document.querySelector("#close-btn");
@@ -15,48 +383,56 @@ function closeNav() {
     sideNav.classList.remove("active");
 }
 
-/*const cards = [
-    {
-        id: 1,
-        title: "The Dark Knight",
-        image: "assets/affiche-large-batman_1.jpg",
-        alt: "Affiche The Dark Knight",
-        categorie: "Drame",
-    },
-    {
-        id: 2,
-        title: "The hobbit",
-        image: "assets/affiche-large-hobbit_1_1.jpg",
-        alt: "Affiche The Hobbit",
-        categorie: "Fantastique",
-    },
-    {
-        id: 3,
-        title: "Harry Potter",
-        image: "assets/affiche-large-batman_1.jpg",
-        alt: "assets/affiche-harry-potter-large_2.jpg",
-        categorie: "Policier",
-    },
-    {
-        id: 4,
-        title: "the dark knight",
-        image: "assets/affiche-large-batman_1.jpg",
-        alt: "affiche the dark knight",
-        categorie: "drame",
-    },
-    {
-        id: 5,
-        title: "the dark knight",
-        image: "assets/affiche-large-batman_1.jpg",
-        alt: "affiche the dark knight",
-        categorie: "drame",
-    },
-    {
-        id: 6,
-        title: "the dark knight",
-        image: "assets/affiche-large-batman_1.jpg",
-        alt: "affiche the dark knight",
-        categorie: "drame",
-    }
-];*/
+// //   <article class=" cards-categories romance">
+// <h1 class="categories-title">Romance</h1>
+// <div class="categories-img">
+//     <img class="categories-img" src="assets/affiches-films/affiche-il-etait-temps.jpg"
+//         alt="Affiche Il etait temps">
+//     <img class="categories-img" src="assets/affiches-films/affiche-me-before-you.jpg"
+//         alt="Affiche Me before you">
+// </div>
+// </article>
 
+
+
+// const kids = document.querySelector(".kids");
+// const fantastic = document.querySelector(".fantastic");
+// const romance = document.querySelector("romance");
+// const action = document.querySelector("action");
+// const comedy = document.querySelector("comedy");
+
+// ==============================
+// une balise articles
+// un h1
+// une div => img
+const section = document.querySelector(".container-categories")
+
+cards.map((card) => {
+
+    const article = document.createElement("article")
+    article.classList.add("cards-categories")
+
+    const title = document.createElement("h1")
+    title.classList.add("categories-title")
+    title.textContent = card.categorie
+
+    article.appendChild(title)
+
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container")
+    article.appendChild(movieContainer)
+
+    card.items.map((item) => {
+
+        const img = document.createElement("img")
+        img.classList.add("categories-img")
+        img.src = item.image
+        img.alt = item.alt
+
+        movieContainer.appendChild(img);
+
+        console.log(img.src)
+
+    })
+    section.appendChild(article)
+})
